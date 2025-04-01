61f1d53e2ce6c4237c6ad275b46029e3cc9af4b8
Aryan Kalia <kalia.aryan2013@gmail.com>
2025-04-01 17:52:58 -0400

    BUGFIX: proxying for frontend debug/hot reload

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -248,28 +248,34 @@ module.exports = composePlugins(
         process.env.MODE === "standalone"
           ? {}
           : {
-              // Port for the Webpack dev server
-              port: HMR_PORT,
-              // Enable HMR
-              hot: true,
-              // Allow cross-origin requests from Django
-              headers: { "Access-Control-Allow-Origin": "*" },
-              static: {
-                directory: path.resolve(__dirname, "../label_studio/core/static/"),
-                publicPath: "/static/",
-              },
-              devMiddleware: {
-                publicPath: `${FRONTEND_HOSTNAME}/react-app/`,
-              },
-              allowedHosts: "all", // Allow access from Django's server
-              proxy: [
-                {
-                  router: {
-                    "/api": `${DJANGO_HOSTNAME}/api`, // Proxy api requests to Django's server
-                  },
-                },
-              ],
+            // Port for the Webpack dev server
+            port: HMR_PORT,
+            // Enable HMR
+            hot: true,
+            // Allow cross-origin requests from Django
+            headers: { "Access-Control-Allow-Origin": "*" },
+            static: {
+              directory: path.resolve(__dirname, "../label_studio/core/static/"),
+              publicPath: "/static/",
             },
+            devMiddleware: {
+              publicPath: `${FRONTEND_HOSTNAME}/react-app/`,
+            },
+            allowedHosts: "all", // Allow access from Django's server
+            proxy: {
+              "/api": {
+                target: `${DJANGO_HOSTNAME}/api`,
+                changeOrigin: true,
+                pathRewrite: { "^/api": "" },
+                secure: false,
+              },
+              "/": {
+                target: `${DJANGO_HOSTNAME}`,
+                changeOrigin: true,
+                secure: false,
+              },
+            },
+          },
     });
   },
 );
